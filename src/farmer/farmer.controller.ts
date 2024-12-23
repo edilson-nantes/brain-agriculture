b2fb5f55105550a26ef013f8053f4d430622677a
Edilson Nantes Junior <nantes.junior@gmail.com>
2024-12-23 17:51:34 -0300

    feat: Create endpoints to get by id and cpf, update and delete  farmers

--- src/farmer/farmer.controller.ts
+++ src/farmer/farmer.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { FarmerService } from './farmer.service';
 import { CreateFarmerDto } from './dtos/createFarmer.dto';
 import { FarmerEntity } from './entities/farmer.entity';
@@ -17,5 +17,25 @@ export class FarmerController {
     @Get()
     async listAllFarmers(): Promise<FarmerEntity[]> {
         return await this.farmerService.listAllFarmers();
+    };
+
+    @Get(':id')
+    async getFarmerById(@Param('id') id: number): Promise<FarmerEntity> {
+        return await this.farmerService.getFarmerById(id);
+    };
+
+    @Get('cpf-cnpj/:cpfCnpj')
+    async getFarmerByCpfCnpj(@Param('cpfCnpj') cpfCnpj: string): Promise<FarmerEntity> {
+        return await this.farmerService.getFarmerByCpfCnpj(cpfCnpj);
+    };
+
+    @Patch(':id')
+    async updateFarmer(@Param('id') id: number, @Body() updateFarmer: CreateFarmerDto): Promise<FarmerEntity> {
+        return await this.farmerService.updateFarmer(id, updateFarmer);
+    };
+
+    @Delete(':id')
+    async deleteFarmer(@Param('id') id: number): Promise<void> {
+        return await this.farmerService.deleteFarmer(id);
     };
 }
--- src/farmer/farmer.service.ts
+++ src/farmer/farmer.service.ts
@@ -1,9 +1,10 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FarmerEntity } from './entities/farmer.entity';
 import { Repository } from 'typeorm';
 import validator from 'cpf-cnpj-validator';
 import { CreateFarmerDto } from './dtos/createFarmer.dto';
+const Joi = require('joi').extend(validator);
 
 @Injectable()
 export class FarmerService {
@@ -14,30 +15,79 @@ export class FarmerService {
     ) {}
 
     async createFarmer(createFarmer: CreateFarmerDto): Promise<FarmerEntity> {
-        try {
-            const Joi = require('joi').extend(validator);
-            const cpfSchema = Joi.document().cpf();
-            const cnpjSchema = Joi.document().cnpj();
+        const cpfSchema = Joi.document().cpf();
+        const cnpjSchema = Joi.document().cnpj();
 
-            if (createFarmer.cpfCnpj.length === 14) {
-                cnpjSchema.validate(createFarmer.cpfCnpj);
-            } else if (createFarmer.cpfCnpj.length === 11) {
-                cpfSchema.validate(createFarmer.cpfCnpj);
-            } else {
-                throw new BadRequestException('Invalid CPF/CNPJ');
-            }
-            
-            return await this.farmerRepository.save(createFarmer);
-        } catch (error) {
-            throw new BadRequestException(error.message);
+        if (createFarmer.cpfCnpj.length === 14) {
+            cnpjSchema.validate(createFarmer.cpfCnpj);
+        } else if (createFarmer.cpfCnpj.length === 11) {
+            cpfSchema.validate(createFarmer.cpfCnpj);
+        } else {
+            throw new BadRequestException('Invalid CPF/CNPJ');
         }
-    };
+        
+        return await this.farmerRepository.save(createFarmer);
+    }
 
     async listAllFarmers(): Promise<FarmerEntity[]> {
-        try {
-            return await this.farmerRepository.find();
-        } catch (error) {
-            throw new Error(error);
+        return await this.farmerRepository.find();
+    }
+
+    async getFarmerById(id: number): Promise<FarmerEntity> {
+        const farmer = await this.farmerRepository.findOne({
+            where: {
+                id: id
+            }
+        });
+        if (!farmer) {
+            throw new NotFoundException('Farmer not found');
         }
-    };
+        return farmer;
+    }
+
+    async getFarmerByCpfCnpj(cpfCnpj: string): Promise<FarmerEntity> {
+        
+        const farmer = await this.farmerRepository.findOne({
+            where: {
+                cpfCnpj: cpfCnpj
+            }
+        });
+
+        if (!farmer) {
+            throw new NotFoundException('Farmer not found');
+        }
+        
+        return farmer;
+    }
+
+    async updateFarmer(id: number, updateFarmer: CreateFarmerDto): Promise<FarmerEntity> {
+        const farmer = await this.farmerRepository.findOne({
+            where: {
+                id: id
+            }
+        });
+
+        if (!farmer) {
+            throw new NotFoundException('Farmer not found');
+        }
+
+        return await this.farmerRepository.save({
+            ...farmer,
+            ...updateFarmer
+        });
+    }
+
+    async deleteFarmer(id: number): Promise<void> {
+        const farmer = await this.farmerRepository.findOne({
+            where: {
+                id: id
+            }
+        });
+
+        if (!farmer) {
+            throw new NotFoundException('Farmer not found');
+        }
+
+        await this.farmerRepository.delete(farmer.id);
+    }
 }
